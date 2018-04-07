90599d9c851de8c37c6175abfd2335864187339f
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2018-04-07 15:51:38 +0300

    Improved cutting

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 from gen_data import prepare, clear, TOOLS, SEED
 from PIL import Image
 import os
-from math import gamma, e
+from math import gamma, e, sqrt
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
@@ -27,9 +27,10 @@ def chi_squared_test(img):
     #     meas_freq[i] /= k
     #     theor_freq[i] /= k
 
-    v = len(meas_freq) - 1
+    v = len(meas_freq)
     chi_sq = sum((meas_freq[i] - theor_freq[i])**2 / theor_freq[i] for i in meas_freq)
-    # prob = 1 - (1 / (2 ** (v / 2) * gamma(v / 2))) * integrate.quad(lambda x: integrand(x, v), 0, chi_sq)[0]
+    print(integrate.quad(lambda x: integrand(x, v), 0, chi_sq))
+    prob = 1 - (1 / (2 ** (v / 2) * gamma(v / 2))) * integrate.quad(lambda x: integrand(x, v), 0, chi_sq)[0]
     return chi_sq
 
 
@@ -50,52 +51,27 @@ def calc_colors(img):
     return amount_dict
 
 
-def crop_4_chuncks(img):
-    chunk1 = img.crop((0, 0, 50, 50))
-    chunk2 = img.crop((50, 0, 100, 50))
-    chunk3 = img.crop((0, 50, 50, 100))
-    chunk4 = img.crop((50, 50, 100, 100))
+def crop_n_chunks(img, n):
+    x = y = int(sqrt(n))
+    piece_x = int(img.size[0] // x)
+    piece_y = int(img.size[1] // y)
+    start_x = 0
+    start_y = 0
+    count = 1
 
-    chunk1.save("chunk1.png")
-    chunk2.save("chunk2.png")
-    chunk3.save("chunk3.png")
-    chunk4.save("chunk4.png")
-
-
-def crop_9_chunks(img):
-    chunk1 = img.crop((0, 0, 30, 30))
-    chunk2 = img.crop((30, 0, 60, 30))
-    chunk3 = img.crop((60, 0, 90, 30))
-
-    chunk4 = img.crop((0, 30, 30, 60))
-    chunk5 = img.crop((30, 30, 60, 60))
-    chunk6 = img.crop((60, 30, 90, 60))
-
-    chunk7 = img.crop((0, 60, 30, 90))
-    chunk8 = img.crop((30, 60, 60, 90))
-    chunk9 = img.crop((60, 60, 90, 90))
-
-    chunk1.save("chunk1.png")
-    chunk2.save("chunk2.png")
-    chunk3.save("chunk3.png")
-    chunk4.save("chunk4.png")
-    chunk5.save("chunk5.png")
-    chunk6.save("chunk6.png")
-    chunk7.save("chunk7.png")
-    chunk8.save("chunk8.png")
-    chunk9.save("chunk9.png")
+    for i in range(y):
+        for j in range(x):
+            chnk = img.crop((start_x, start_y, start_x + piece_x, start_y + piece_y))
+            chnk.save("chunk" + str(count) + ".png")
+            start_x += piece_x
+            count += 1
+        start_y += piece_y
+        start_x = 0
 
 
-def del_9_chunks():
-    os.remove("chunk1.png")
-    os.remove("chunk2.png")
-    os.remove("chunk3.png")
-    os.remove("chunk4.png")
-    os.remove("chunk5.png")
-    os.remove("chunk6.png")
-    os.remove("chunk7.png")
-    os.remove("chunk8.png")
-    os.remove("chunk9.png")
+def del_n_chunks(n):
+    for i in range(n):
+        os.remove("chunk" + str(i + 1) + ".png")
 
 
 def analyze():
@@ -104,11 +80,11 @@ def analyze():
         os.chdir(tool)
         for s in SEED:
             img = Image.open(str(s) + ".png")
-            crop_9_chunks(img)
+            crop_n_chunks(img, 9)
             for i in range(1, 10):
                 chnk = Image.open("chunk" + str(i) + ".png")
-                print(s, i, chi_squared_test(chnk))
-            del_9_chunks()
+                chi_squared_test(chnk)
+            del_n_chunks(9)
         os.chdir("..")
 
 
