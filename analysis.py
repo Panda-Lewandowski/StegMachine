90abf55c4a178cf72d8ff4998b39debe9e65e1e8
Panda-Lewandowski <daria-shcherbatyuk@yandex.ru>
2018-04-08 01:14:02 +0300

    Added graphs

--- analysis.py
+++ analysis.py
@@ -2,7 +2,10 @@ from gen_data import prepare, clear, TOOLS, SEED
 from PIL import Image
 import os
 from math import sqrt
-from scipy.stats import chisquare
+from scipy import stats, mean
+import matplotlib.pyplot as plt
+
+NUM_OF_CHUNCKS = 25
 
 
 def chi_squared_test(img):
@@ -11,11 +14,19 @@ def chi_squared_test(img):
     meas_freq_g = calc_colors(img, channel='g')
     meas_freq_b = calc_colors(img, channel='b')
 
-    theor_freq = {x: k/256 for x in range(256)}
+    theor_freq = {x: 1/256 for x in range(256)}
 
-    chi, pval = chisquare([meas_freq_r[x] for x in meas_freq_r.keys()],
-                          [theor_freq[x] for x in theor_freq.keys()])
-    print(chi, pval)
+    chis = [0, 0, 0]
+    probs = [0, 0, 0]
+
+    chis[0], probs[0] = stats.chisquare([meas_freq_r[x] for x in meas_freq_r.keys()],
+                                        [theor_freq[x] for x in theor_freq.keys()])
+    chis[1], probs[1] = stats.chisquare([meas_freq_g[x] for x in meas_freq_g.keys()],
+                                        [theor_freq[x] for x in theor_freq.keys()])
+    chis[2], probs[2] = stats.chisquare([meas_freq_b[x] for x in meas_freq_b.keys()],
+                                        [theor_freq[x] for x in theor_freq.keys()])
+
+    return chis, probs
 
 
 def calc_colors(img, channel='r'):
@@ -62,15 +73,41 @@ def del_n_chunks(n):
 def analyze():
     for tool in TOOLS:
         os.chdir(tool)
+
+        fig, axs = plt.subplots(1, 4, tight_layout=True)
+
+        list_of_chuncks = []
+        img = Image.open("pure.png")
+        crop_n_chunks(img, NUM_OF_CHUNCKS)
+        for i in range(1, NUM_OF_CHUNCKS + 1):
+            chnk = Image.open("chunk" + str(i) + ".png")
+            list_of_chuncks.append(mean(chi_squared_test(chnk)[1]))
+
+        del_n_chunks(NUM_OF_CHUNCKS)
+        axs[0].bar([i+1 for i in range(NUM_OF_CHUNCKS)], height=list_of_chuncks)
+        axs[0].set_title("Pure image")
+        axs[0].set_ylabel("Probability")
+
         for s in SEED:
+            list_of_chuncks = []
             img = Image.open(str(s) + ".png")
-            crop_n_chunks(img, 9)
-            for i in range(1, 10):
+            crop_n_chunks(img, NUM_OF_CHUNCKS)
+            for i in range(1, NUM_OF_CHUNCKS + 1):
                 chnk = Image.open("chunk" + str(i) + ".png")
-                print("\n\n_______chunk" + str(i) + ".png_________")
-                chi_squared_test(chnk)
-            del_n_chunks(9)
+                list_of_chuncks.append(mean(chi_squared_test(chnk)[1]))
+            if s == 10:
+                axs[1].set_title('Seed 10')
+                axs[1].bar([i + 1 for i in range(NUM_OF_CHUNCKS)], height=list_of_chuncks)
+            if s == 100:
+                axs[2].set_title('Seed 100')
+                axs[2].bar([i + 1 for i in range(NUM_OF_CHUNCKS)], height=list_of_chuncks)
+            if s == 200:
+                axs[3].set_title('Seed 200')
+                axs[3].bar([i + 1 for i in range(NUM_OF_CHUNCKS)], height=list_of_chuncks)
+
+            del_n_chunks(NUM_OF_CHUNCKS)
         os.chdir("..")
+        plt.show()
 
 
 if __name__ == "__main__":
