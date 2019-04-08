8d76c4250c4e973de39982633f807159b50e36ac
Kirillova Sofia <s.kirillovaaa@gmail.com>
2019-04-08 17:57:58 +0300

    Add tests

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class WorkFlowTest(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual('Hello wold!', 'Hello wold!')
+
+
+if __name__ == '__main__':
+    unittest.main()
